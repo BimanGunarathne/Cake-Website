b6e516d8794305b54f1bcc3945ae6ddc7b07445a
Biman Gunarathne <bimangunarathne@outlook.com>
2023-10-10 19:09:30 +0530

    Configure the database and add table to the database

---
+++ server/models/Users.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, DataTypes) => {
+    const Posts = sequelize.define('Posts', {
+        firstName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        lastName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    })
+    return Posts
+}
