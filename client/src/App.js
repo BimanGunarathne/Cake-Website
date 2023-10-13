b941924407a2d2dbdc11053c42d2d6f844d204e4
Biman Gunarathne <bimangunarathne@outlook.com>
2023-10-13 18:58:38 +0530

    added some user UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+import axios from "axios";
+import { useEffect, useState } from 'react';
 
 function App() {
-  return (
-    <div className="App"></div>
-  );
+  const [listOfUsers, setListOfUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/users").then((response) => {
+      setListOfUsers(response.data);
+    });
+  }, []);
+  return <div className="App">{listOfUsers.map((value, key) => {
+    return <div className="user">
+        <div className="name">{value.name}</div>
+        <div className="body">{value.username}</div>
+        <div className="footer">{value.email}</div>
+      </div>
+  })}
+  </div>
 }
 
 export default App;
