0d6564e5e3dfe35c31c3adcdbb35338d166a04b3
Biman Gunarathne <bimangunarathne@outlook.com>
2023-10-15 07:52:18 +0530

    create a basic admin

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
+import Admin from './pages/Admin';
+import RegesterAdmins from './pages/RegesterAdmins';
 
 function App() {
-  
+
 
   return (
     <div className='App'>
-      
+      <Router>
+        <Link to="/registeradmins">Register Admins</Link>
+        <Link to="/">Admin Page</Link>
+        <Routes>
+          <Route path="/" exact Component={Admin} />
+          <Route path="/registeradmins" exact Component={RegesterAdmins} />
+        </Routes>
+      </Router>
     </div>
-  )
+  );
 }
 
 export default App;
---
+++ client/src/pages/RegesterAdmins.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function RegesterAdmins() {
+    return <div>RegesterAdmins</div>
+}
+
+export default RegesterAdmins
